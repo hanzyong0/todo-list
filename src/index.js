16f0fd72c11f566573472f1ea93010d68d90ae37
Zhi Yong Han <hanzyong0@gmail.com>
2022-01-09 00:07:10 +0800

    configure firebase hosting

--- src/index.js
+++ src/index.js
@@ -3,6 +3,25 @@ import * as buttons from './buttons.js';
 import { todoList, projectList } from './item';
 import { displayTodoList } from './todo';
 import { displayProjectList } from './project';
+import { initializeApp } from 'firebase/app';
+
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCRBZnKcNGKfYZABz_32nOHO0bpUZ1Bb7A",
+
+    authDomain: "to-do-list-32a5c.firebaseapp.com",
+
+    projectId: "to-do-list-32a5c",
+
+    storageBucket: "to-do-list-32a5c.appspot.com",
+
+    messagingSenderId: "892417758519",
+
+    appId: "1:892417758519:web:c818df30a53b25829136b2"
+
+};
+
+const app = initializeApp(firebaseConfig);
 
 (() => {
     const a = JSON.parse(localStorage.getItem('todoList'));
